0c6ff2461d2a2d1d59fd0a00f7d2dfb0c8189dc1
Ndikubwayo-albert <ndialbert45@gmail.com>
2023-03-28 11:55:59 +0200

    settings

--- simpleweb/settings.py
+++ simpleweb/settings.py
@@ -97,17 +97,19 @@ DATABASES ={
     'default': dj_database_url.config(
         conn_max_age=600,
         conn_health_checks=True,
-    ),         
+    ), 
+    
+    # 'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #         'USER': '',
+    #         'PASSWORD': '',
+    #         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+    #         'PORT': '',  
+    #     }        
 }
 
-# 'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#             'USER': '',
-#             'PASSWORD': '',
-#             'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#             'PORT': '',  
-#         }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
